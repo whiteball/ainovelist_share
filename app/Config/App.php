176d719729b55cfa37ad094f3229c90d57c7b8e0
whiteball <white_ball_11@hotmail.com>
2022-01-28 10:18:01 +0900

    設定を追加

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://localhost:8081/';
 
     /**
      * --------------------------------------------------------------------------
@@ -108,7 +108,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $appTimezone = 'America/Chicago';
+    public $appTimezone = 'Asia/Tokyo';
 
     /**
      * --------------------------------------------------------------------------
