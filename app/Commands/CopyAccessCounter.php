5b5b3ffac7bb7ffff33773fafb8869fe30813ed1
whiteball <white_ball_11@hotmail.com>
2022-04-11 01:05:27 +0900

    ランキングが作成されていないときはアクセスカウンターのスナップショットを更新しないように変更。

--- app/Commands/CopyAccessCounter.php
+++ app/Commands/CopyAccessCounter.php
@@ -3,7 +3,9 @@
 namespace App\Commands;
 
 use App\Models\Prompt_access_snapshot;
+use App\Models\Ranking;
 use CodeIgniter\CLI\BaseCommand;
+use DateTime;
 
 class CopyAccessCounter extends BaseCommand
 {
@@ -16,6 +18,17 @@ class CopyAccessCounter extends BaseCommand
 
     public function run(array $params)
     {
+        /** @var Ranking */
+        $ranking = model(Ranking::class);
+        $date    = new DateTime();
+        $result  = $ranking->where('date', $date->format('Y-m-d'))->selectCount('*', 'count')->get();
+        if (! ($result && $result->getRow()->count > 0)) {
+            echo 'error: admin:copy_access_counter: ranking not exist.';
+            log_message('error', __METHOD__ . ': 本日付のランキングが存在しません');
+
+            return;
+        }
+
         /** @var Prompt_access_snapshot */
         $access = model(Prompt_access_snapshot::class);
         $access->copy();
