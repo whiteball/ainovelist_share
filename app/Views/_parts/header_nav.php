6635d62192f6dfdaf060f9b017af86d2ddca02cf
whiteball <white_ball_11@hotmail.com>
2023-01-30 13:05:10 +0900

    画面幅が狭いときのナビゲーションヘッダをコンパクトにした。

--- app/Views/_parts/header_nav.php
+++ app/Views/_parts/header_nav.php
@@ -1,10 +1,10 @@
 <div class="container">
-	<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-		<div class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-			<a href="<?= site_url('/')?>" class="nav-link px-2 link-dark">AIのべりすと プロンプト共有</a>
+	<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-md-3 mb-4 border-bottom">
+		<div class="d-flex align-items-center col-3 col-sm-2 col-md-3 mb-0 text-dark text-decoration-none" style="word-break: keep-all;">
+			<a href="<?= site_url('/')?>" class="nav-link px-2 link-dark text-center">AIのべりすと<wbr> プロンプト共有</a>
 		</div>
 
-		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+		<ul class="nav col-5 col-sm-6 col-md-6 ps-sm-4 ps-md-0 justify-content-center">
 			<li><a href="<?= site_url('/about')?>" class="nav-link px-2 link-dark">サイトについて</a></li>
 			<li class="d-none d-lg-inline"><a href="<?= site_url('/tags')?>" class="nav-link px-2 link-dark">タグ一覧</a></li>
 			<li class="d-none d-lg-inline"><a href="<?= site_url('/ranking')?>" class="nav-link px-2 link-dark">ランキング</a></li>
@@ -23,7 +23,7 @@
 			</li>
 		</ul>
 
-		<div class="col-md-3 text-end">
+		<div class="col-4 col-md-3 text-end">
 			<?php if (isset($_SESSION['login'])): ?>
 				<a type="button" class="btn btn-outline-success me-2 btn-sm" href="<?= site_url('create') ?>">投稿</a>
 				<a type="button" class="btn btn-outline-info me-2 btn-sm" href="<?= site_url('mypage') ?>">マイページ</a>
